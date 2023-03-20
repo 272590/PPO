3c248166e1e7ec62baceb0bc5bee70a755d55f97
nG4ZD4 <124004979+nG4ZD4@users.noreply.github.com>
2023-03-20 15:24:01 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ui.h>
+#include <studenci.h>
+using namespace std;
+
+int main()
+{
+    ui();
+    studenci();
+
+    return 0;
+}
---
+++ ui.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void ui()
+{
+    cout << "hello" << endl;
+}
