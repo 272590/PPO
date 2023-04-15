48e55e07b62532786b2a8fd68c048cf1a1690917
nG4ZD4 <124004979+nG4ZD4@users.noreply.github.com>
2023-04-15 12:54:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
 #include <ui.h>
-#include <studenci.h>
+#include <dodaj.h>
+#include <wypisz.h>
+#include <vector>
+#include <string>
 using namespace std;
+
+class Student {
+public:
+    string imie;
+    string nazwisko;
+    int nr_indeksu;
+    int wiek;
+
+    Student(string i, string n, int nr, int w) {
+        imie = i;
+        nazwisko = n;
+        nr_indeksu = nr;
+        wiek = w;
+    }
+};
 
 int main()
 {
     ui();
-    dodawanie_studenci();
-    drukowanie_studenci();
+    dodajStudenta();
+    wypiszStudentow();
+    
+    vector<Student> studenci;
+
+    char wybor;
+    do {
+        cout << "Co chcesz zrobic?" << endl;
+        cout << "1 - Dodaj studenta" << endl;
+        cout << "2 - Wypisz studentow" << endl;
+        cout << "3 - Zakończ program" << endl;
+        cin >> wybor;
+        
+         switch(wybor) {
+            case '1':
+                dodajStudenta(studenci);
+                break;
+            case '2':
+                wypiszStudentow(studenci);
+                break;
+            case '3':
+                break;
+            default:
+                cout << "Error" << endl;
+                break;
+        }
+    } while(wybor != '3');
 
     return 0;
 }
